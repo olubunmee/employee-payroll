0fa739477bc09e767e7471c571d44ba509736043
Corporate Admin <iswcorp@ISW-211230-320.Interswitchng.com>
2023-08-18 15:44:49 +0100

    first change on dev branch

--- src/main/java/com/interswitch/ng/employeepayroll/TestApp.java
+++ src/main/java/com/interswitch/ng/employeepayroll/TestApp.java
@@ -1,2 +1,12 @@
-package com.interswitch.ng.employeepayroll;public class TestApp {
+package com.interswitch.ng.employeepayroll;
+
+public class TestApp {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int subtract(int a, int b){
+        return a - b;
+    }
 }
